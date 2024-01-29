b9096a906e9b9983657b9164fc3ff57c971e8545
Aaron <bauhd@gmx.net>
2024-01-29 03:50:34 +0100

    chore: initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://github.com/RealBauHD">
+        <img src="https://avatars.githubusercontent.com/u/71191102?v=4" alt="logo" class="logo"/>
+    </a>
+  </div>
+`
